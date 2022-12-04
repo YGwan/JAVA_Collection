2f75d4ac0ddae2f061035797567e247ffa241fcc
YongGwan <32181771@dankook.ac.kr>
2022-12-04 18:29:24 +0900

    feat : LinkedList의 시작부분에 값을 추가하는 로직 구현

--- src/linkedList/LinkedListMain.java
+++ src/linkedList/LinkedListMain.java
@@ -3,6 +3,9 @@ package linkedList;
 public class LinkedListMain {
 
     public static void main(String[] args) {
-        System.out.println("LinkedList 구현");
+        System.out.println("############ LinkedList ############");
+        LinkedList numbers = new LinkedList();
+        numbers.addFirst(1);
+        System.out.println(numbers);
     }
 }
